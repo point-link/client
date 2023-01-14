904eac7ae30b9fd2d4d83e583d03f27e8de3f56d
mys1024 <mys.1024@outlook.com>
2023-01-15 03:03:21 +0800

    feat: 添加 Element Plus

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { router } from '~/router'
 import App from '~/App.vue'
 
+import 'element-plus/dist/index.css'
+
 const app = createApp(App)
 
 app.use(router)
--- src/renderer/pages/index.vue
+++ src/renderer/pages/index.vue
@@ -1,5 +1,16 @@
+<script lang="ts" setup>
+import { ElButton } from 'element-plus'
+
+function onClick() {
+  alert('Hi!')
+}
+</script>
+
 <template>
   <div>
     Hello, MiiXinn!
+    <ElButton @click="onClick">
+      Hi!
+    </ElButton>
   </div>
 </template>
