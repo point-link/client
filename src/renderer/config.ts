567f75154ddcbaa7c96a64b205d7dcc12a27895f
mys1024 <mys.1024@outlook.com>
2023-01-19 05:47:24 +0800

    refactor: 从本地加载默认头像

--- src/renderer/config.ts
+++ src/renderer/config.ts
@@ -1,2 +1,2 @@
 export const API_BASE_PATH = 'https://api.pointlink.top'
-export const DEFAULT_AVATAR = 'https://miixinn-1303077031.cos.ap-guangzhou.myqcloud.com/public/avatar/default.jpg'
+export const DEFAULT_AVATAR = '/avatar/default.jpg'
