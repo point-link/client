c538e3be6d59080dd1c406b119ad037ca332e0d1
mys1024 <mys.1024@outlook.com>
2023-01-19 06:02:16 +0800

    refactor: 优化检查网络环境的时机

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,3 +1,10 @@
+<script lang="ts" setup>
+import { useNetworkStore } from '~/stores/network'
+
+// 进入 app 后立即加载 network store
+useNetworkStore()
+</script>
+
 <template>
   <RouterView v-slot="{ Component }">
     <KeepAlive>
