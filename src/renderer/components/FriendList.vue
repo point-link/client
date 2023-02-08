a078512e074cb1ec70e9d9927871d115ae9f08e1
mys1024 <mys.1024@outlook.com>
2023-02-08 19:37:06 +0800

    feat: 在线状态

--- src/renderer/components/FriendList.vue
+++ src/renderer/components/FriendList.vue
@@ -3,16 +3,32 @@ import { storeToRefs } from 'pinia'
 import { useRouter } from 'vue-router'
 import { useFriendStore } from '~/stores/friend'
 import { useChatStore } from '~/stores/chat'
-import { DEFAULT_AVATAR_URL } from '~/config'
+import { useNetworkStore } from '~/stores/network'
+import { DEFAULT_AVATAR_URL, DISPLAY_MODE_ENABLE } from '~/config'
 import type { Friend } from '~/typings/app'
 
 const router = useRouter()
-const { friends } = storeToRefs(useFriendStore())
+const { networkInfo } = storeToRefs(useNetworkStore())
+const { friends, friendOnlineClients } = storeToRefs(useFriendStore())
 const { selectedFriend } = storeToRefs(useChatStore())
 
 function openChatroom(friend: Friend) {
   selectedFriend.value = friend
   router.replace('/main/chat/chatroom')
+}
+
+function getFriendStatus(uid: number) {
+  const client = friendOnlineClients.value[uid]
+  if (!client)
+    return 'offline'
+  else if (
+    (client.ipv6 && networkInfo.value.ipv6)
+      || (client.ipv4 && networkInfo.value.ipv4)
+      || DISPLAY_MODE_ENABLE
+  )
+    return 'online-linkable'
+  else
+    return 'online-unlinkable'
 }
 </script>
 
@@ -21,13 +37,18 @@ function openChatroom(friend: Friend) {
     <div
       v-for="friend of friends" :key="friend.uid"
       p-2 flex items-center space-x-2
+      border-l-4
       cursor-pointer
       :class="{
         'bg-gray-300': selectedFriend?.uid === friend.uid,
+        'border-gray': getFriendStatus(friend.uid) === 'offline',
+        'border-yellow': getFriendStatus(friend.uid) === 'online-unlinkable',
+        'border-green': getFriendStatus(friend.uid) === 'online-linkable',
       }"
       @click="openChatroom(friend)"
     >
       <div>
+        {{ }}
         <img
           w-10 aspect-1
           :src="friend.profile.avatar ? friend.profile.avatar : DEFAULT_AVATAR_URL"
